f2147d299038118a074893bf7a497a2b90dc376a
ThongHM2 <thonghm2@fpt.com>
2025-03-05 22:22:21 +0700

    fix(config): update budgets

--- apps/angular-store/src/app/layouts/layout-slides/layout-slides.component.ts
+++ apps/angular-store/src/app/layouts/layout-slides/layout-slides.component.ts
@@ -1,12 +1,11 @@
 import { Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SliderComponent } from '../../components/slider/slider.component';
-import { CalendarComponent } from "../../components/calendar/calendar.component";
 import { NzSafeAny } from 'ng-zorro-antd/core/types';
 
 @Component({
   selector: 'app-layout-slides',
-  imports: [CommonModule, SliderComponent, CalendarComponent],
+  imports: [CommonModule, SliderComponent],
   templateUrl: './layout-slides.component.html',
   styleUrl: './layout-slides.component.scss',
 })
