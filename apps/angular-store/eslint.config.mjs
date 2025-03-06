980c4eea3272fd39c328c66ced919f34a1acfda1
ThinhLH7 <thinhlh7@fpt.com>
2025-03-06 13:17:41 +0700

    a

--- apps/angular-store/eslint.config.mjs
+++ apps/angular-store/eslint.config.mjs
@@ -1,7 +1,7 @@
 import nx from '@nx/eslint-plugin';
 import baseConfig from '../../eslint.config.mjs';
 
-export default [
+module.exports = [
   ...baseConfig,
   ...nx.configs['flat/angular'],
   ...nx.configs['flat/angular-template'],
